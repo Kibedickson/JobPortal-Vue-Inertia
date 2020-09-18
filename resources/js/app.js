7d0df7e47a71ed66f1fe9458a95ee721cde41eee
kibedickson <kibedickson3@gmail.com>
2020-09-18 12:25:11 +0300

    Initial Commit

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+import { InertiaApp } from "@inertiajs/inertia-vue"
+import Vue from 'vue'
+
+require('./bootstrap')
+Vue.use(InertiaApp)
+Vue.config.productionTip = false
+
+const app = document.getElementById('app')
+
+new Vue({
+    render: h => h(InertiaApp, {
+        props: {
+            initialPage: JSON.parse(app.dataset.page),
+            resolveComponent: name => require(`./Pages/${name}`).default,
+        },
+    }),
+}).$mount(app)
