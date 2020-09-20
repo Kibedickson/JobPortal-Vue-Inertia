6edbb5f76edb4c0d3dca1a8c14f8ca518b7ff74c
Dickson <kibedickson3@gmail.com>
2020-09-20 11:04:43 +0300

    Home controller to render home component

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class HomeController extends Controller
 {
@@ -13,16 +16,16 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return Response
      */
     public function index()
     {
-        return view('home');
+        return Inertia::render('Home');
     }
 }
