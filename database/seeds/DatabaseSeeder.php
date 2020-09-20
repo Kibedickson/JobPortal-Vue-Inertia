6e1f796db411b52b040562978220e11320cd4d5d
Dickson <kibedickson3@gmail.com>
2020-09-20 12:53:38 +0300

    Job and Role user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             RoleSeeder::class,
+            JobSeeder::class,
+            RoleUserSeeder::class,
             ]);
     }
 }
